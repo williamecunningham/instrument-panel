b789c17ebbf5dbc1a8c26ddec729be244a976dc5
Bill Cunningham <williamecunningham@gmail.com>
2026-09-14 16:08:32 -0500

    Added array of sensors to include RPM and Oil Pressure to the Fuel status

--- sensor.js
+++ sensor.js
@@ -1,11 +1,26 @@
-// Create and a Variable:
-let fuelLevel = 100;
+const sensors = [
+        { name: "Fuel", value: 100, unit: "%" },
+        { name: "RPM", value: 2400, unit: "rpm" },
+        { name: "Oil Pressure", value: 45, unit: "psi" }
+];
+
 
 function tick(){
-        fuelLevel -= 2;
-        if(fuelLevel <= 0){
-                clearInterval(timerId);
+        for (const s of sensors) {
+                if(s.name == 'Fuel') {
+                        s.value -= 2
+                        if(s.value <= 0){
+                                clearInterval(timerId);}
+                }
+                if(s.name == 'RPM'){
+                        if(s.value <= 2500 && s.value >= 2200){
+                                s.value -=5}
+                        else if (s.value <= 2200){
+                                s.value +=2}}
+                if(s.name == "Oil Pressure"){
+                                {s.value -= 1}}
+                console.log(s.name, s.value, s.unit);
+
         }
-        console.log(`Fuel: ${fuelLevel}`);      
 }
 const timerId = setInterval(tick, 1000);
